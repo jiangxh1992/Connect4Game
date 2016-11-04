539bad6b608fe6f76e6815eb745e5e17b697eb3e
Xinhou Jiang <xinhoujiang@Xinhous-MacBook-Pro.local>
2016-11-04 22:55:53 +0800

    完成所有基本逻辑；界面简单优化；

--- LSRConect4/app/src/main/java/com/example/lsr/lsrconect4/Main2Activity.java
+++ LSRConect4/app/src/main/java/com/example/lsr/lsrconect4/Main2Activity.java
@@ -8,28 +8,29 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import java.util.ArrayList;
-
-import static com.example.lsr.lsrconect4.R.id.table;
 
 public class Main2Activity extends AppCompatActivity {
 
-    // 棋盘状态
+    // 当前下棋的玩家
     private int turn = 1;
+    // 游戏状态
     private int result = 0;
-    // 棋盘状态
+    // 棋盘数据model
     private int chessState[][] = null;
     // 棋子堆栈
-    private ArrayList STATCK = null;
+    private ArrayList<Point> STATCK = null;
+    // 记录四个方向是否有4个以上相同棋子
+    boolean[] winDir = null;
 
-    // UI
+    // 获取UI引用
     private Button button_restart;
     private Button button_retract;
     private ImageView red;
     private ImageView green;
     private TextView text;
 
+    // 棋子id
     int[] idList = {R.id.btn0, R.id.btn1, R.id.btn2, R.id.btn3, R.id.btn4, R.id.btn5, R.id.btn6,
             R.id.btn7, R.id.btn8, R.id.btn9, R.id.btn10, R.id.btn11, R.id.btn12, R.id.btn13,
             R.id.btn14, R.id.btn15, R.id.btn16, R.id.btn17, R.id.btn18, R.id.btn19, R.id.btn20,
@@ -42,34 +43,43 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        // activity初始化
         init();
-
+        // 新游戏初始化
         newGameInit();
     }
 
-    // 只初始化一次
+    /**
+     * activity初始化
+     */
     void init() {
 
+        // 为棋子按钮注册点击事件
         for (int i=1 ; i<=42 ; i++) {
             ImageButton btn = (ImageButton)findViewById(idList[i-1]);
             btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
+                    // 如果游戏结束不响应
                     if (result == 0)
                         return;
+                    // 计算点击所在列数
                     int id = view.getId();
                     int col = getCol(id);
+                    // 下一步棋
                     GoStep(col);
                 }
             });
         }
 
+        // UI初始化
         button_restart = (Button) findViewById(R.id.restart);
         button_retract = (Button) findViewById(R.id.retarct);
         red = (ImageView) findViewById(R.id.red);
         green = (ImageView) findViewById(R.id.green);
         text = (TextView)findViewById(R.id.tip);
 
+        // 注册重新开始游戏事件
         button_restart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -77,34 +87,46 @@ public class Main2Activity extends AppCompatActivity {
             }
         });
 
+        // 注册退棋事件
         button_retract.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (result == 0)
+                    return;
+                goback();
             }
         });
 
     }
 
+    /**
+     * 重新开始游戏初始化
+     */
     void newGameInit() {
+
+        /** 清空UI **/
         text.setText("");
-        result = 1;
-        // 默认红色先手
-        turn = 1;
         red.setImageResource(R.drawable.red_wint);
-        // 清空棋盘
-        chessState = null;
-        // 重新初始化
-        chessState = new int[6][7];
+        green.setImageResource(R.drawable.green_t);
         for(int i=0 ; i<42 ; i++) {
             ImageButton btn = (ImageButton) findViewById(idList[i]);
             btn.setImageResource(R.drawable.empty_t);
         }
+
+        /** 清空数据  **/
+        result = 1;
+        turn = 1;
+        // 清空棋盘
+        chessState = null;
+        chessState = new int[6][7];
         // 清空栈
         STATCK = null;
-        STATCK = new ArrayList();
+        STATCK = new ArrayList<Point>();
     }
 
+    /**
+     * 根据按钮id计算列数
+     */
     int getCol(int id) {
         for (int i=0 ; i<42 ; i++) {
             if (idList[i] == id)
@@ -114,38 +136,28 @@ public class Main2Activity extends AppCompatActivity {
         return 10000;
     }
 
+    /**
+     * 下棋
+     */
     void GoStep(int col) {
 
+        // 从下往上寻找空闲位置，从而确定下棋具体位置
         for (int i=5 ; i>=0 ; i--) {
             if(chessState[i][col] == 0) {
-
-                // ui
-                ImageButton btn = (ImageButton)findViewById(idList[i*7+col]);
-                if (turn == 1){
-                    btn.setImageResource(R.drawable.red_t);
-                    green.setImageResource(R.drawable.green_wint);
-                    red.setImageResource(R.drawable.red_t);
-                    chessState[i][col] = turn;
-                    turn = 2;
-                }else {
-                    btn.setImageResource(R.drawable.green_t);
-                    red.setImageResource(R.drawable.red_wint);
-                    green.setImageResource(R.drawable.green_t);
-                    chessState[i][col] = turn;
-                    turn = 1;
-                }
-
-                //
+                // 添加到棋盘数据模型
+                chessState[i][col] = turn;
+                // 入栈
                 STATCK.add(new Point(i,col));
+
+                // 平局
                 if (STATCK.size() == 42){
-                    // draw
                     text.setText("Draw!");
                     turn = 0;
                     result = 0;
                     return;
                 }
 
-                // 判断是否有人赢
+                // 判断有人赢
                 if (judge(i,col)){
                     if (turn == 1) {
                         text.setText("red!");
@@ -155,53 +167,185 @@ public class Main2Activity extends AppCompatActivity {
                         text.setText("green!");
                         result = 0;
                     }
+                    // 显示获胜棋子
+                    showRes(i,col);
+                    return;
+                }
+
+                // 无人赢切换玩家
+                ImageButton btn = (ImageButton)findViewById(idList[i*7+col]);
+                if (turn == 1){
+                    btn.setImageResource(R.drawable.red_t);
+                    green.setImageResource(R.drawable.green_wint);
+                    red.setImageResource(R.drawable.red_t);
+                    turn = 2;
+                }else {
+                    btn.setImageResource(R.drawable.green_t);
+                    red.setImageResource(R.drawable.red_wint);
+                    green.setImageResource(R.drawable.green_t);
+                    turn = 1;
                 }
 
                 return;
             }
         }
-
     }
 
-    // 判断是否赢
+    /**
+     * 判断是否赢
+     */
     boolean judge(int y, int x) {
         int count = 0;
+        boolean res = false;
+        winDir = null;
+        winDir = new boolean[4];
         // horizontal
         for (int i =0;i<7;i++){
             if (chessState[y][i] == turn) {
                 ++count;
-                if (count == 4)
-                    return true;
+                if (count >= 4) {
+                    res = true;
+                    winDir[0] = true;
+                }
             }
             else
                 count = 0;
         }
+
         // vertical
         count = 0;
         for (int i =0;i<6;i++){
-            if (chessState[i][y] == turn) {
+            if (chessState[i][x] == turn) {
                 ++count;
-                if (count == 4)
-                    return true;
+                if (count >= 4) {
+                    res = true;
+                    winDir[1] = true;
+                }
             }
             else
                 count = 0;
         }
 
+        // mdiagnal
+        count = 0;
+        int min = x>y ? y : x;
+        int ox = x - min;
+        int oy = y - min;
+        for (int i=0; ; i++) {
+            if(ox+i>6 || oy+i>5)
+                break;
+            if (chessState[oy+i][ox+i] == turn) {
+                ++count;
+                if (count >= 4) {
+                    res = true;
+                    winDir[2] = true;
+                }
+            }
+            else
+                count = 0;
+        }
 
+        // adiagnal
+        count = 0;
+        min = x>(5-y) ? 5-y : x;
+        ox = x - min;
+        oy = y + min;
+        for (int i=0;  ;i++) {
+            if(ox+i>6 || oy-i<0)
+                break;
+            if (chessState[oy-i][ox+i] == turn) {
+                ++count;
+                if (count >= 4) {
+                    res = true;
+                    winDir[3] = true;
+                }
+            }
+            else
+                count = 0;
+        }
 
-        return false;
+        return res;
     }
 
-    // 退棋
-    void goback(int y, int x) {
+    /**
+     * 退棋
+     */
+    void goback() {
         if (STATCK==null || STATCK.size() == 0)
             return;
+        Point p = STATCK.remove(STATCK.size()-1);
+        chessState[p.x][p.y] = 0;
+        // 切换玩家
+        ImageButton btn = (ImageButton)findViewById(idList[p.x*7+p.y]);
+        if (turn == 1){
+            btn.setImageResource(R.drawable.empty_t);
+            green.setImageResource(R.drawable.green_wint);
+            red.setImageResource(R.drawable.red_t);
+            turn = 2;
+        }else {
+            btn.setImageResource(R.drawable.empty_t);
+            red.setImageResource(R.drawable.red_wint);
+            green.setImageResource(R.drawable.green_t);
+            turn = 1;
+        }
+
     }
 
-    // 特殊显示获胜的棋子
+    /**
+     * 特殊显示获胜的棋子
+     */
     void showRes(int y, int x) {
+        // 遍历搜索，以当前坐标为中心连续的全部显示为特殊棋子
 
+        // 当前棋子
+        showPos(y*7+x);
+
+        int step = 1;
+        if(winDir[0]) {
+            // 右
+            while (x+step<=6 && chessState[y][x+step] == turn) {showPos(y*7+(x+step));step++;}
+            // 左
+            step = 1;
+            while (x-step>=0 && chessState[y][x-step] == turn) {showPos(y*7+(x-step));step++;}
+        }
+
+        if (winDir[1]) {
+            // 上
+            step = 1;
+            while (y-step>=0 && chessState[y-step][x] == turn) {showPos((y-step)*7+x);step++;}
+            // 下
+            step = 1;
+            while (y+step<=5 && chessState[y+step][x] == turn) {showPos((y+step)*7+x);step++;}
+        }
+
+        if (winDir[2]) {
+            // 左上
+            step = 1;
+            while (x-step>=0 && y-step>=0 && chessState[y-step][x-step] == turn) {showPos((y-step)*7+(x-step));step++;}
+            // 右下
+            step = 1;
+            while (x+step<=6 && y+step<=5 && chessState[y+step][x+step] == turn) {showPos((y+step)*7+(x+step));step++;}
+        }
+
+        if (winDir[3]) {
+            // 左下
+            step = 1;
+            while (x-step>=0 && y+step<=5 && chessState[y+step][x-step] == turn) {showPos((y+step)*7+(x-step));step++;}
+            // 右上
+            step = 1;
+            while (x+step<=6 && y-step>=0 && chessState[y-step][x+step] == turn) {showPos((y-step)*7+(x+step));step++;}
+        }
+
+    }
+
+    void showPos(int id) {
+
+        ImageButton btn = (ImageButton) findViewById(idList[id]);
+        // 更换图片
+        if (turn == 1)
+            btn.setImageResource(R.drawable.red_wint);
+        else
+            btn.setImageResource(R.drawable.green_wint);
     }
 
 }
